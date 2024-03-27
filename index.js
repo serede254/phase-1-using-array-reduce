bbe568cf93a1115c18136484da3b3e09d3db2112
root <root@SURFACE.>
2024-03-27 04:10:40 +0300

    reduce

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-
+let totalBatteries = batteryBatches.reduce((acc,element) => acc+element,0);
+console.log(totalBatteries());
 // Code your solution here
